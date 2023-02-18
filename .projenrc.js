c0418103779bc4b230797f09ef022f023d6376f3
Zlatko <senovlato@outlook.com>
2023-02-18 18:43:52 +0300

    feat: projen initial structure

---
+++ .projenrc.js
@@ -0,0 +1,45 @@
+const { awscdk, TextFile } = require("projen");
+const { DependabotRegistryType } = require("projen/lib/github");
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: "mobworx-streamer",
+  repository: "https://github.com/sehovizko/mobwrox-streamer.git",
+  keywords: ["hls", "streaming", "golang"],
+
+  prettier: true,
+  codeCov: true,
+  release: true,
+  defaultReleaseBranch: "main",
+  workflowNodeVersion: "18",
+  cdkVersion: "2.65.0",
+
+  renovatebot: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["renovatebot[bot]"],
+  },
+  license: false,
+
+  lambdaOptions: {
+    runtime: awscdk.LambdaRuntime.NODEJS_18_X,
+  },
+});
+
+new TextFile(project, ".nvmrc", {
+  marker: false,
+  lines: ["v18"],
+});
+
+new TextFile(project, ".editorconfig", {
+  marker: false,
+  lines: `root = true
+[*]
+charset = utf-8
+insert_final_newline = true
+trim_trailing_whitespace = true
+indent_size = 2
+tab_width = 2
+`.split("\n"),
+});
+
+project.synth();
