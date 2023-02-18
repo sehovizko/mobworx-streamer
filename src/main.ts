ab2ca786036cb2233f3581c300151c78c8c02832
Zlatko <95984933+sehovizko@users.noreply.github.com>
2023-02-18 20:24:10 +0300

    feat: projen initial structure (#1)
    * feat: projen initial structure
    
    * fix: remove codecov from projenrc
    
    * feat: add endpoint lambdas
    
    * chore: remove test files

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { App, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { EndpointNestedStack } from "./endpoint/endpoint.nested-stack";
+
+export class StreamerStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+    new EndpointNestedStack(this, "EndpointNestedStack");
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new StreamerStack(app, "mobworx-streamer-dev", { env: devEnv });
+// new MyStack(app, 'mobworx-streamer-prod', { env: prodEnv });
+
+app.synth();
