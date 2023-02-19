d5cdab92b04073b3bd0d256c2ae7a4344c19a5b1
Zlatko <senovlato@outlook.com>
2023-02-19 15:25:34 +0300

    fix(endpoint): mstats error response

--- src/endpoint/mstats/query-munit-stats.go
+++ src/endpoint/mstats/query-munit-stats.go
@@ -23,14 +23,30 @@ var redisClient *redis.Client
 func HandleQueryMunitStats(ctx aws.Context, _ events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	mStats, err := redisClient.ZRange(ctx, "munitstatsidx", MunitStatsIDXStart, MunitStatsIDXEnd).Result()
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,GET",
+			},
+			Body: err.Error(),
+		}, nil
 	}
 
 	body, err := json.Marshal(MunitStats{
 		Data: mStats,
 	})
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,GET",
+			},
+			Body: err.Error(),
+		}, err
 	}
 
 	return events.APIGatewayProxyResponse{
