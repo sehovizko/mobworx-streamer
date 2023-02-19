7c9aaa7168e7a52cd026fe8861112f6f3c7c2c88
Zlatko <95984933+sehovizko@users.noreply.github.com>
2023-02-20 00:55:01 +0300

    feat(streaming): utils added. (#5)
    * Update streamer-connection.go
    
    * types.go added
    
    * update streamer-connection.go
    
    * created DumpToS3 method.
    
    * PR Comments Solved
    
    * pr comments resolved
    
    * utils updated
    
    * utils test (GetSignailAction)
    
    * utils test (Ack)
    
    * fix: dumpToS3
    
    * utils test (DumpToS3)
    
    * refactor: added additional assertions

--- streaming/helpers/utils.go
+++ streaming/helpers/utils.go
@@ -1,1 +1,218 @@
 package helpers
+
+import (
+	b64 "encoding/base64"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/google/uuid"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Utils struct {
+	StreamerConnection *StreamerConnection
+	S3UserBucked       string
+}
+
+func NewUtils(streamerConnection *StreamerConnection) *Utils {
+	var utils Utils
+	utils.StreamerConnection = streamerConnection
+	utils.S3UserBucked = os.Getenv("S3_USER_BUCKET")
+	return &utils
+}
+
+type Message struct {
+	Payload   *Payload
+	TimeStamp string
+	Action    SignalAction
+}
+
+type Payload struct {
+	Segment      *Segment
+	VideoSegment *VideoSegment
+	AudioSegment *AudioSegment
+	Part         *Part
+	VideoPart    *VideoPart
+	AudioPart    *AudioPart
+}
+
+type Segment struct {
+	Mapping *Mapping
+	Data    []byte
+}
+
+type VideoSegment struct {
+	Mapping *Mapping
+	Data    []byte
+}
+
+type AudioSegment struct {
+	Mapping *Mapping
+	Data    []byte
+}
+
+type Part struct {
+	Data []byte
+}
+
+type VideoPart struct {
+	Data []byte
+}
+
+type AudioPart struct {
+	Data []byte
+}
+
+type Mapping struct {
+	Data []byte
+}
+
+type Response struct {
+	Action    string
+	Version   int
+	Id        string
+	Timestamp string
+	Size      int
+	Latency   int
+	Payload   *Payload
+}
+
+func (u *Utils) GetAckSignal(event *events.APIGatewayProxyRequest, uploadLatency int) *Response {
+	msg := new(Message)
+	body := []byte(event.Body)
+
+	var err error
+	if event.IsBase64Encoded {
+		//Todo:This converting is possibly wrong.
+		data := b64.StdEncoding.EncodeToString(body)
+		err = json.Unmarshal([]byte(data), &msg)
+	} else {
+		err = json.Unmarshal(body, &msg)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	payload := msg.Payload
+	ackAction := "unknown"
+	switch msg.Action {
+	case UpdateVariant:
+		ackAction = string(AckVariant)
+		payload.Segment.Mapping = nil
+		break
+	case UpdateRendition:
+		ackAction = string(AckRendition)
+		payload.Segment.Mapping = nil
+		break
+	case UpdateSegment:
+		ackAction = string(AckSegment)
+		payload.Segment.Data = nil
+		if payload.Segment.Mapping != nil {
+			payload.Segment.Mapping.Data = nil
+		}
+	case UpdatePart:
+		ackAction = string(AckPart)
+		payload.Part.Data = nil
+		if payload.Segment != nil && payload.Segment.Mapping != nil {
+			payload.Segment.Mapping.Data = nil
+		}
+		break
+	case UpdateDemuxSegment:
+		ackAction = string(AckDemuxSegment)
+		payload.VideoSegment.Data = nil
+		if payload.VideoSegment.Mapping != nil {
+			payload.VideoSegment.Mapping.Data = nil
+		}
+		payload.AudioSegment.Data = nil
+		if payload.AudioSegment.Mapping != nil {
+			payload.AudioSegment.Mapping.Data = nil
+		}
+		break
+	case UpdateDemuxPart:
+		ackAction = string(AckDemuxPart)
+		payload.VideoPart.Data = nil
+		if payload.VideoSegment != nil && payload.VideoSegment.Mapping != nil {
+			payload.VideoSegment.Mapping.Data = nil
+		}
+		payload.AudioPart.Data = nil
+		if payload.AudioSegment != nil && payload.AudioSegment.Mapping != nil {
+			payload.AudioSegment.Mapping.Data = nil
+		}
+		break
+	case Ping:
+		ackAction = string(Pong)
+		break
+	case Abort:
+		ackAction = string(Aborted)
+		break
+	case Terminate:
+		ackAction = string(Terminated)
+		break
+	default:
+		break
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+	size := len(jsonPayload)
+	//Latency := uploadLatency || ((new Date).getTime() - Number(msg.Timestamp));
+	return &Response{
+		Action:    ackAction,
+		Version:   1,
+		Id:        uuid.New().String(),
+		Timestamp: strconv.FormatInt(time.Now().Unix(), 10),
+		Size:      size,
+		Latency:   uploadLatency, //TODO: Should be Latency variable.
+		Payload:   payload,
+	}
+}
+
+func (u *Utils) Ack(event *events.APIGatewayWebsocketProxyRequest, uploadLatency int) error {
+	tempData, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	proxyRequest := new(events.APIGatewayProxyRequest)
+	err = json.Unmarshal(tempData, proxyRequest)
+	if err != nil {
+		return err
+	}
+
+	data := u.GetAckSignal(proxyRequest, uploadLatency)
+	connectionId := event.RequestContext.ConnectionID
+	connection := NewStreamerConnection(u.StreamerConnection.Session, event.RequestContext.DomainName, event.RequestContext.Stage)
+	dataByte, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	startTime := time.Now().UnixMilli()
+	_, err = connection.PostData(dataByte, connectionId)
+	if err != nil {
+		return err
+	}
+	endTime := time.Now().UnixMilli()
+
+	log.Printf("%+v\n", *data)
+	log.Println("ack postData totalTime in ms: ", endTime-startTime)
+	return nil
+}
+
+func (u *Utils) DumpToS3(key string, data []byte) (*s3.PutObjectOutput, error) {
+	svc := s3.New(u.StreamerConnection.Session, aws.NewConfig().WithRegion("us-west-2"))
+	putObject := &s3.PutObjectInput{
+		ACL:    aws.String("public-read"),
+		Body:   aws.ReadSeekCloser(strings.NewReader(string(data))),
+		Bucket: aws.String(u.S3UserBucked),
+		Key:    aws.String(key),
+	}
+	return svc.PutObject(putObject)
+}
---
+++ streaming/helpers/utils_test.go
@@ -0,0 +1,136 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestGetAckSignal(t *testing.T) {
+	part := &Part{Data: []byte("part data")}
+	videoPart := &VideoPart{Data: nil}
+	audioPart := &AudioPart{Data: nil}
+	mapping := &Mapping{Data: []byte("map Data")}
+	videoSegment := &VideoSegment{
+		Mapping: mapping,
+		Data:    []byte("VideoSegment Data"),
+	}
+	audioSegment := &AudioSegment{
+		Mapping: mapping,
+		Data:    []byte("AudioSegment Data"),
+	}
+	segment := &Segment{
+		Mapping: mapping,
+		Data:    []byte("Segment Data"),
+	}
+	payload := &Payload{
+		Segment:      segment,
+		VideoSegment: videoSegment,
+		AudioSegment: audioSegment,
+		Part:         part,
+		VideoPart:    videoPart,
+		AudioPart:    audioPart,
+	}
+	message := &Message{
+		Payload:   payload,
+		TimeStamp: strconv.FormatInt(time.Now().UnixMilli(), 10),
+		Action:    UpdatePart,
+	}
+
+	mySession := session.Must(session.NewSession())
+	sc := NewStreamerConnection(mySession, "domain", "stage")
+	utils := NewUtils(sc)
+
+	messageByte, _ := json.Marshal(*message)
+
+	request := &events.APIGatewayProxyRequest{Body: string(messageByte), IsBase64Encoded: false}
+
+	testCases := []struct {
+		event   *events.APIGatewayProxyRequest
+		latency int
+		resp    *Response
+	}{
+		{
+			event:   request,
+			latency: 10,
+			resp: &Response{
+				Action: string(AckPart),
+			},
+		},
+	}
+
+	for i, c := range testCases {
+		t.Run(fmt.Sprintf("SubTest %d", i+1), func(t *testing.T) {
+			resp := utils.GetAckSignal(c.event, c.latency)
+			assert.Equal(t, c.resp.Action, resp.Action)
+		})
+	}
+}
+
+func TestAck(t *testing.T) {
+	part := &Part{Data: []byte("part data")}
+	videoPart := &VideoPart{Data: nil}
+	audioPart := &AudioPart{Data: nil}
+	mapping := &Mapping{Data: []byte("map Data")}
+	videoSegment := &VideoSegment{
+		Mapping: mapping,
+		Data:    []byte("VideoSegment Data"),
+	}
+	audioSegment := &AudioSegment{
+		Mapping: mapping,
+		Data:    []byte("AudioSegment Data"),
+	}
+	segment := &Segment{
+		Mapping: mapping,
+		Data:    []byte("Segment Data"),
+	}
+	payload := &Payload{
+		Segment:      segment,
+		VideoSegment: videoSegment,
+		AudioSegment: audioSegment,
+		Part:         part,
+		VideoPart:    videoPart,
+		AudioPart:    audioPart,
+	}
+	message := &Message{
+		Payload:   payload,
+		TimeStamp: strconv.FormatInt(time.Now().UnixMilli(), 10),
+		Action:    UpdatePart,
+	}
+
+	mySession := session.Must(session.NewSession())
+	sc := NewStreamerConnection(mySession, "domain", "stage")
+	utils := NewUtils(sc)
+
+	messageByte, _ := json.Marshal(*message)
+	request := &events.APIGatewayWebsocketProxyRequest{
+		Body:            string(messageByte),
+		IsBase64Encoded: false,
+		RequestContext: events.APIGatewayWebsocketProxyRequestContext{
+			Stage:        "stg",
+			ConnectionID: "555",
+			DomainName:   "dmn",
+		},
+	}
+
+	err := utils.Ack(request, 10)
+	require.NoError(t, err)
+	//Todo: Should write assertions to here.
+}
+
+func TestDumpToS3(t *testing.T) {
+	mySession := session.Must(session.NewSession())
+	sc := NewStreamerConnection(mySession, "domain", "stage")
+	utils := NewUtils(sc)
+	utils.S3UserBucked = "S3_USER_BUCKET"
+	response, err := utils.DumpToS3("key", []byte("data"))
+	require.NoError(t, err)
+	fmt.Println(response)
+	//Todo: Should write assertions to here.
+}
